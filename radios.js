fb1f22bb21e232e3f0c689a20ba486853e0249b9
Glitch (hello-express) <none>
2020-05-22 16:00:37 +0000

    🎮🐽 Checkpoint ./server.js:1076526/1 ./radios.js:1076526/129

--- radios.js
+++ radios.js
@@ -1,10 +1,12 @@
 module.exports = {
   'risefm': {
     type: 'AzuraCast',
-    endpoint: 'https://radio.risefm.net/api/nowplaying/1'
+    endpoint: 'https://radio.risefm.net/api/nowplaying/1',
+    stream: 'https://radio.risefm.net/radio/8000/radio.mp3'
   },
   'bounce': {
     type: 'Bounce',
-    endpoint: 'https://stats.boun.cc/'
+    endpoint: 'https://stats.boun.cc/',
+    stream: 'https://live.boun.cc'
   }
 };
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/nowplaying", (req, res) => {
-  res.json(Object.keys(APIs););
+  res.json(Object.keys(APIs));
 });
 
 app.get("/api/nowplaying/:radio", async (req, res) => {
