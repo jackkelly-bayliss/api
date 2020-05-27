c04771c867a089afc32feb8d145a375c33fb6e38
Glitch (hello-express) <none>
2020-05-27 22:27:04 +0000

    🌧🍂 Checkpoint ./radios.js:1076526/754

--- radios.js
+++ radios.js
@@ -1,17 +1,12 @@
 module.exports = {
-  'risefm': {
-    type: 'AzuraCast',
-    endpoint: 'https://radio.risefm.net/api/nowplaying/1',
-    stream: 'https://radio.risefm.net/radio/8000/radio.mp3'
+  risefm: {
+    type: "AzuraCast",
+    endpoint: "https://radio.risefm.net/api/nowplaying/1",
+    stream: "https://radio.risefm.net/radio/8000/radio.mp3"
   },
-  'bounce': {
-    type: 'Bounce',
-    endpoint: 'https://stats.boun.cc/',
-    stream: 'https://live.boun.cc'
-  },
-  'nova': {
-    type: 'AzuraCast',
-    endpoint: 'https://azuracast.livida.net/api/nowplaying/1',
-    stream: 'https://azuracast.livida.net/radio/8000/radio.mp3'
+  bounce: {
+    type: "Bounce",
+    endpoint: "https://stats.boun.cc/",
+    stream: "https://live.boun.cc"
   }
 };
