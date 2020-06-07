0963762c24ea1c910799eb0edb83c513d354befc
Joshua Pemberton <joshp2865@gmail.com>
2020-06-07 12:32:48 +0100

    update api?
    please fix if bad

--- radios.js
+++ radios.js
@@ -3,5 +3,9 @@ module.exports = {
     type: 'Bounce',
     endpoint: 'https://stats.boun.cc/',
     stream: 'https://live.boun.cc'
+  },
+  keyfm: {
+    type: 'KeyFM',
+    endpoint: 'https://api.keyfm.net/stats'
   }
 }
